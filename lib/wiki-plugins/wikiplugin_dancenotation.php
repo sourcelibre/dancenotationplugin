3cc675d29e76a04dd98b85f3b866fcb20014731f
Alexandre Quessy <alexandre@quessy.net>
2014-09-04 04:54:08 -0400

    display a basic table

--- lib/wiki-plugins/wikiplugin_dancenotation.php
+++ lib/wiki-plugins/wikiplugin_dancenotation.php
@@ -5,6 +5,115 @@
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
 // $Id: wikiplugin_split.php 51581 2014-06-05 14:26:44Z lphuberdeau $
 
+function _count_counts($moves, $countspermove)
+{
+	$counts = 0;
+
+	foreach ($moves as $move)
+	{
+		if (isset($move['counts']))
+		{
+			$counts += $moves['counts'];
+		} else {
+			$counts += $countspermove;
+		}
+	}
+	return $counts;
+}
+
+function _get_tracks($moves)
+{
+	$tracks = array();
+
+	foreach ($moves as $move)
+	{
+		foreach ($move as $k => $v)
+		{
+			if ($k == 'title')
+			{
+				// pass
+			} else {
+				if (! in_array($k, $tracks))
+				{
+					$tracks[] = $k;
+				}
+			}
+		}
+	}
+	return $tracks;
+}
+
+function _chart_to_html($chart, $countspermove=8, $countsperline=16)
+{
+	$ret = '';
+	$moves = array();
+	$count = 0;
+	$totalcounts = 0;
+	$tracks = array();
+
+	//$title = '';
+	// if (isset($chart['title']);
+	// {
+	// 	$title = $chart['title'];
+	// }
+
+	$ret .= '<table class="table">';
+	if (isset($chart['moves']) && is_array($chart['moves']))
+	{
+		$moves = $chart['moves'];
+		$totalcounts = _count_counts($moves, $countspermove);
+		$all_tracks = _get_tracks($chart);
+
+		foreach ($moves as $move)
+		{
+			if (($count % $countsperline) == 0)
+			{
+				if ($count == 0)
+				{
+					$ret .= "<tr>";
+				} else {
+					$ret .= "</tr>\n<tr>";
+				}
+			}
+			$counts_in_this_move = $countspermove;
+			if (isset($move['counts']))
+			{
+				$counts_in_this_move += $moves['counts'];
+			}
+			if (isset($move['steps']))
+			{
+				for ($i = 0; $i < $counts_in_this_move; $i++)
+				{
+					$ret .= "<td>";
+
+					// title of this move
+					if ($i == 0 && isset($move['title']))
+					{
+						$ret .= $move['title'] . ' ';
+					}
+
+					// each step of this move
+					if (isset($move['steps'][$i]))
+					{
+						$ret .= $move['steps'][$i];
+					}
+					$ret .= "</td>";
+				}
+			}
+
+			$count++;
+		}
+	}
+
+	if (($count % $countsperline) != 1)
+	{
+		$ret .= "</tr>\n";
+	}
+	$ret .= "</table>\n";
+
+	return '~np~' . $ret . '~/np~';
+}
+
 function wikiplugin_dancenotation_info()
 {
 	return array(
@@ -15,8 +124,34 @@ function wikiplugin_dancenotation_info()
 		'filter' => 'wikicontent',
 		'icon' => 'img/icons/new.png',
 		'tags' => array( 'basic' ),
-		'params' => array(),
+		'params' => array(
+			'countsperline' => array(
+				'required' => false,
+				'name' => tra('Counts per line'),
+				'description' => tra('Width of each line in the chart, in counts.'),
+				'filter' => 'int',
+				'advanced' => false,
+				'default' => '16',
+			),
+			'countspermove' => array(
+				'required' => false,
+				'name' => tra('Default counts per move'),
+				'description' => tra('Default duration of each move, in counts.'),
+				'filter' => 'int',
+				'advanced' => false,
+				'default' => '8',
+			),
+        ),
 	);
+}
+
+function _get_print_r($loaded)
+{
+	$ret = "";
+	$ret .= "<code>\n";
+	$ret .= print_r($loaded, true);
+	$ret .= "\n</code>\n";
+	return $ret;
 }
 
 /*
@@ -24,15 +159,15 @@ function wikiplugin_dancenotation_info()
  */
 function wikiplugin_dancenotation($data, $params, $pos)
 {
-    require_once 'lib/core/Horde/Yaml.php';
-    require_once 'lib/core/Horde/Yaml/Loader.php';
-    require_once 'lib/core/Horde/Yaml/Node.php';
+	require_once 'lib/core/Horde/Yaml.php';
+	require_once 'lib/core/Horde/Yaml/Loader.php';
+	require_once 'lib/core/Horde/Yaml/Node.php';
 
-    $loaded = Horde_Yaml::load($data);
-    $ret = "";
-    $ret .= "<code>\n";
-    $ret .= print_r($loaded, true);
-    $ret .= "\n</code>\n";
-    return $ret;
+	$loaded = Horde_Yaml::load($data);
+	$ret = "";
+	//$ret .= _get_print_r($loaded);
+	$ret .= _chart_to_html($loaded);
+
+	return $ret;
 }
 
